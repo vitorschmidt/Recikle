fd91f3d81c10cc2de13e3a4f45f407c871ca3d75
Vinicius <vinicius.stmartins@gmail.com>
2022-09-06 14:11:37 -0300

    adjust:schedule

--- schedule_collects/serializers.py
+++ schedule_collects/serializers.py
@@ -1,26 +1,34 @@
+from dataclasses import field
 from materials.serializers import ListMaterialSerializer
 from rest_framework import serializers
 from rest_framework.exceptions import APIException
 from users.serializers import UserSerializer
 from schedule_collects.models import ScheduleCollect
 from users.serializers import UserSerializer
-
+import ipdb
 
 class UniqueValidationError(APIException):
     status_code = 422
 
 
-class InfoCollectSerializer(serializers.ModelSerializer):
-    materials = ListMaterialSerializer(read_only=True)
+class ScheduleSerializer(serializers.ModelSerializer):
+    # materials = ListMaterialSerializer(read_only=True)
     user_id = UserSerializer(read_only=True)
-    
+
     class Meta:
         model = ScheduleCollect
-        fields = ["id", "days", "material", "city", "user_id"]
-        read_only_fields = ["id","user_id","material"]
+        fields = ["id", "days", "city", "user_id"]
+        read_only_fields = ["id"]
 
 
     def create(self, validated_data):
-        info_collect = ScheduleCollect.objects.create(**validated_data)
+        material_pop = validated_data.pop("materials")
+        # ipdb.set_trace()
+        schedule = ScheduleCollect.objects.get_or_create(**validated_data)[0]
+        schedule.materials.add(material_pop)
+        return schedule
 
-        return info_collect
+class ListScheduleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ScheduleCollect
+        fields = "__all__"
--- schedule_collects/views.py
+++ schedule_collects/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework import generics
+from django.shortcuts import get_object_or_404
+from schedule_collects.mixins import SerializerByMethodMixin
+from schedule_collects.models import ScheduleCollect
+from schedule_collects.serializers import ScheduleSerializer,ListScheduleSerializer
+from materials.models import Material
 
-# Create your views here.
+def get_object_by_id(model, **kwargs):
+    object= get_object_or_404(model,**kwargs)
+    return object
+
+class ScheduleView(SerializerByMethodMixin,generics.ListCreateAPIView):
+    queryset=ScheduleCollect.objects.all()
+    serializer_map = {"GET":ListScheduleSerializer,"POST":ScheduleSerializer}
+    lookup_url_kwarg="id"
+
+    def get_queryset(self):
+        material_id = self.kwargs["id"]
+        material = get_object_by_id(Material, id=material_id)
+        schedule = ScheduleCollect.objects.filter(materials=material)
+        return schedule
+
+    def perform_create(self, serializer):
+        material_id = self.kwargs["id"]
+        material=get_object_or_404(Material, id=material_id)
+        serializer.save(materials=material)
+
+class ScheduleDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ScheduleSerializer
+    queryset = ScheduleCollect.objects.all()
+
+    lookup_url_kwarg="schedule_id"
