da6a0e4855b83d8827c1805a9f115b8d5d75995b
Vitor Schmidt <schmidt_vitor@hotmail.com>
2022-09-06 11:12:35 -0300

    fix: infor collect models

--- info_collects/models.py
+++ info_collects/models.py
@@ -4,6 +4,8 @@ from django.db import models
 
 class InfoCollect(models.Model):
     user_id = models.ManyToManyField("users.User", related_name="info_collect")
-    localization = models.CharField(max_length=128)
+    cep = models.IntegerField()
+    address = models.CharField(max_length=120)
+    reference_point = models.CharField(max_length=120)
     materials = models.ManyToManyField("materials.Material", related_name="info_collects")
     company = models.ForeignKey('companies.Company', on_delete = models.CASCADE, related_name = 'info_collect')
