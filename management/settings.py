2ce4409516a4c447a87de545cb7d41a6fc9f64cc
Vinicius <vinicius.stmartins@gmail.com>
2022-09-09 11:50:59 -0300

    heroku
    Signed-off-by: Vinicius <vinicius.stmartins@gmail.com>

--- management/settings.py
+++ management/settings.py
@@ -16,7 +16,7 @@ import django_on_heroku
 import dotenv
 
 dotenv.load_dotenv()
-django_on_heroku.settings(locals())
+django_on_heroku.settings(locals(),staticfiles=False)
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
