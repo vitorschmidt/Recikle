670896b425516ac5b6b56284fae496eb4d049443
Henrique <Henriferreira99@gmail.com>
2022-09-12 11:06:35 -0300

    model fixes

--- info_companies/models.py
+++ info_companies/models.py
@@ -3,5 +3,5 @@ from django.db import models
 class InfoCompany(models.Model):
     telephone = models.IntegerField()
     email = models.EmailField()
-    address = models.CharField(max_length=100)
+    address = models.CharField(max_length=150)
     company = models.ForeignKey('companies.Company', on_delete = models.CASCADE, related_name = 'info_company')
